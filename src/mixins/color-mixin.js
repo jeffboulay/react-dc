2d9d899a954f7a4f60972ad49faa17b1e3f9fb2d
Jeff Boulay <Jeff.boulay@gmail.com>
2018-07-10 19:58:23 -0500

    added prop-type

--- src/mixins/color-mixin.js
+++ src/mixins/color-mixin.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import {withProps} from '../utils'
-
-const {arrayOf, func, number, string} = React.PropTypes
+import React from "react";
+import { withProps } from "../utils";
+import PropTypes from "prop-types";
+const { arrayOf, func, number, string } = PropTypes;
 
 export default withProps({
-  colorAccessor: func,
-  colorDomain: arrayOf(string),
-  colors: func, // TO DO : shape a scale
-  linearColors: arrayOf(number),
-  ordinalColors: arrayOf(string)
-})
+    colorAccessor: func,
+    colorDomain: arrayOf(string),
+    colors: func, // TO DO : shape a scale
+    linearColors: arrayOf(number),
+    ordinalColors: arrayOf(string)
+});
